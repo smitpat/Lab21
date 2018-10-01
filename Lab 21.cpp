5bc14b54e9c12e60c4cdd4e425c0c3a5144c2964
smitpat <email.smitpatel@gmail.com>
2018-10-01 13:33:33 -0400

    Lab21 Main
    Main cpp file for lab21

---
+++ Lab 21.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void swap(int* a, int* b) {
+    int tmp = *b;
+    *b= *a;
+    *a = tmp;
+}
+
+int main()
+{
+  int a, b;
+  cout<<"Please enter two integers: "<<endl;
+  cin >> a >>b;
+  cout<< "The original a and b are " a <<" and "<< b<<endl;
+  swap(&a, &b);
+  cout<<"After swap, the a and b are "<< a <<" and "<< b<<endl;
+}
